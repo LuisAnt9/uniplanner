5198cae2f21ada7de8a24a3794b3cd91f3cc48d7
aclarakttym11 <aclaraoliveira11k@gmail.com>
2026-06-10 12:01:24 -0300

    Add forgot password route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, me, updateProfile } = require("../controllers/authController");
+const { register, login, me, updateProfile, forgotPassword } = require("../controllers/authController");
 const { requireAuth } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", requireAuth, me);
 router.put("/profile", requireAuth, updateProfile);
+router.post("/forgot-password", forgotPassword);
 
 module.exports = router;
